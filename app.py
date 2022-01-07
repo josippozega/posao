671420e373191ee61d2bd9c6ef2dc53a0d65b7e4
josippozega <96910194+josippozega@users.noreply.github.com>
2022-01-07 23:56:36 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import requests
+
+
+def main():
+    menu = ["Početna", "O Aplikaciji"]
+    choice = st.sidebar.selectbox("Menu", menu)
+
+    st.title("App za traženje posla, dev bj Josip Požega")
+
+    if choice =="Početna":
+        st.subheader("Početna")
+
+
+    else:
+        st.subheader("O Aplikaciji")
+
+
+
+
+if __name__ = 'main':
+   main()
